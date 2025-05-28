505c968ef898e4e1da64bf2d5ad5e5312b521aab
Alissonperirajs <alisson.pereirasj@gmail.com>
2025-05-28 20:00:36 +0000

    Implementação para Questão 1 do nível fácil

---
+++ Questoes Nivel Facil/Questao1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    struct aluno{
+        char Nome;
+        int idade;
+        float notaFinal;
+    };
+
+    struct aluno Nome;
+    printf("\n----------Cadastro de Aluno----------\n\n\n");
+    printf("Nome do Aluno:");
+    fflush(stdin);
+    fgets(aluno.Nome, 40)
+
+}
