ed704d14054e4f2419636f2760a339839f921162
Alissonperirajs <alisson.pereirasj@gmail.com>
2025-05-30 16:33:24 +0000

    Implementação de Struct e também da entrada de informações

--- Questoes Nivel Facil/Questao1.c
+++ Questoes Nivel Facil/Questao1.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
+#define TAM 50
+#define NOTA 10
+
+typedef struct {
+    char nome;
+    int idade;
+    float notaFinal;
+} aluno1;
 
 int main(){
+    int iCont = 0;
+    char nome[TAM];
+    int idade[TAM];
+    float nota1, nota2, nota3, notaFinal[NOTA];
+    aluno1 listaAluno;
 
-    struct aluno{
-        char Nome;
-        int idade;
-        float notaFinal;
-    };
+    printf("\n=== Cadastro do Aluno ===\n");
+    printf("Digite o nome do Aluno: ");
+    scanf("%49s", nome);
+    printf("\nDigite a idade do aluno: ");
+    scanf("%d", idade);
+    printf("\nDigite a nota das tres provas do aluno para definir a nota final:");
+    scanf("%f %f %f", &nota1, &nota2, &nota3);
 
-    struct aluno Nome;
-    printf("\n----------Cadastro de Aluno----------\n\n\n");
-    printf("Nome do Aluno:");
-    fflush(stdin);
-    fgets(aluno.Nome, 40)
-
+    return 0;
 }
